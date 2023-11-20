7084468e6292f5d43c2ed39a1ccb777dae5bab60
Enno Richter <enno@nerdworks.de>
2023-11-20 12:56:25 +0100

    python3Packages.llama-cpp-python: init at 0.2.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6406,6 +6406,8 @@ self: super: with self; {
 
   lizard = callPackage ../development/python-modules/lizard { };
 
+  llama-cpp-python = callPackage ../development/python-modules/llama-cpp-python { };
+
   llfuse = callPackage ../development/python-modules/llfuse {
     inherit (pkgs) fuse;
   };
